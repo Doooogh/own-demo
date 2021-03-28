f8b9b8f9757416c018cc30fce462fdcfa757d524
li <m13283354149@163.com>
2021-03-28 08:20:13 +0800

    first commit

---
+++ redis-demo/src/main/java/com/example/redis/config/RedisConfig.java
@@ -0,0 +1,30 @@
+package com.example.redis.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+
+
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.port}")
+    private int port;
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+    @Value("${spring.redis.password}")
+    private String password;
+    @Value("${spring.redis.pool.max-active}")
+    private int maxActive;
+    @Value("${spring.redis.pool.max-wait}")
+    private int maxWait;
+    @Value("${spring.redis.pool.max-idle}")
+    private int maxIdle;
+    @Value("${spring.redis.pool.min-idle}")
+    private int minIdle;
+
+}
